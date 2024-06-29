162456210abe5778a8f8f29f017a309a1c73d1e2
Samu3l4175 <samuel.iuele@icloud.com>
2024-06-29 22:40:06 +0200

    test

--- lib/stories/stories.dart
+++ lib/stories/stories.dart
@@ -1,31 +1,26 @@
 import 'package:flutter/material.dart';
 
-List<Scaffold> stories = [
-  Scaffold(
-    body: Column(
-      children: [
-        Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage('assets/images/test.jpg'),
+class StoryPage extends StatelessWidget {
+  const StoryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget storyPage = Scaffold(
+      body: Column(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/images/test.jpg'),
+              ),
             ),
           ),
-        ),
-        Text('TEST')
-      ],
-    ),
-  ),
-  Scaffold(
-    body: Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          fit: BoxFit.cover,
-          image: NetworkImage(
-            "https://i.pinimg.com/originals/2e/c6/b5/2ec6b5e14fe0cba0cb0aa5d2caeeccc6.jpg",
-          ),
-        ),
+          Text('TEST')
+        ],
       ),
-    ),
-  ),
-];
+    );
+
+    return storyPage;
+  }
+}
